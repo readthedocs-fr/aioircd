287278f1236e3ac7f0954921b09c215cf4244687
Julien Castiaux <julien.castiaux@gmail.com>
2020-08-16 22:27:47 +0200

    Documentation

--- aioircd.py
+++ aioircd.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+""" A minimalist asynchronous IRC server """
+
 import argparse
 import asyncio
 import functools
@@ -13,9 +15,16 @@ from abc import ABCMeta, abstractmethod
 nick_re = re.compile(r"[a-zA-Z][a-zA-Z0-9\-_]{0,8}")
 chann_re = re.compile(r"#[a-zA-Z0-9\-_]{1,49}")
 
+IOLevel = logging.INFO + 1
+logging.addLevelName(IOLevel, 'IO')
 logger = logging.getLogger(__name__)
 
 def main():
+    """
+    Application entrypoint, extract configuration from argparse,
+    configure logging and run the server loop.
+    """
+
     cli = argparse.ArgumentParser()
     cli.add_argument('--host', type=str, default='::1')
     cli.add_argument('--port', type=int, default=6667)
@@ -23,11 +32,8 @@ def main():
     cli.add_argument('-s', '--silent', action='count', default=0)
     options = cli.parse_args()
 
-    logging.MSG = logging.INFO + 1
-    logging.addLevelName(logging.MSG, 'MSG')
-    logging.msg = functools.partial(logging.log, logging.MSG)
-    logger.msg = functools.partial(logger.log, logging.MSG)
-
+    # Color the [LEVEL] part of messages, need new terminal on Windows
+    # https://github.com/odoo/odoo/blob/13.0/odoo/netsvc.py#L57-L100
     class ColoredFormatter(logging.Formatter):
         def format(self, record):
             fg, bg = {
@@ -41,19 +47,22 @@ def main():
             record.levelname = f"\033[1;{fg}m\033[1;{bg}m{record.levelname}\033[0m"
             return super().format(record)
 
-
-    verbosity = 10 * max(0, min(3 - options.verbose + options.silent, 5))
+    # Reset logging to ONLY log messages on stdout
     stdout = logging.StreamHandler()
-    stdout.formatter = (ColoredFormatter if os.name != 'nt' else logging.Formatter)(
+    stdout.formatter = ColoredFormatter(
         "%(asctime)s [%(levelname)s] <%(funcName)s> %(message)s"
     )
     logging.root.handlers.clear()
     logging.root.addHandler(stdout)
+
+    # Set the verbosity, enables python level warnings on -vvv
+    verbosity = 10 * max(0, min(3 - options.verbose + options.silent, 5))
     logging.root.setLevel(verbosity)
     if verbosity == 0:
         logging.captureWarnings(True)
         warnings.filterwarnings('default')
 
+    # Start the server on foreground, gracefully quit on the first SIGINT
     server = Server(options.host, options.port)
     try:
         asyncio.run(server.serve())
@@ -74,8 +83,10 @@ class Server:
         self.local = ServerLocal()
         self._serv = None
 
-    async def serve(self):
+    async def serve_forever(self):
+        """ Start listening for new connections """
         self._serv = await asyncio.start_server(self.handle, self.host, self.port)
+
         logger.info("Listening on %s port %i", self.host, self.port)
         async with self._serv:
             await self._serv.serve_forever()
@@ -91,13 +102,14 @@ class Server:
         raise SystemExit(0)
 
     async def handle(self, reader, writer):
+        """ Create a new User and serve him until he disconnects or we kick him """
         peeraddr = writer.get_extra_info('peername')[0]
         logger.info("New connection from %s", peeraddr)
         user = User(self.local, reader, writer)
         try:
             await user.serve()
         except Exception:
-            logger.exception("Fatal error in client loop !")
+            logger.exception("Error in client loop !")
         finally:
             writer.close()
             await writer.wait_closed()
@@ -106,6 +118,7 @@ class Server:
 
 
 class ServerLocal:
+    """ Storage shared among all server's entities """
     def __init__(self):
         self.users = {}
         self.channels = {}
@@ -156,7 +169,6 @@ class User:
         self.writer = writer
         self.state = UserConnectedState(self)
         self._nick = None
-
         self.channels = []
 
     @property
@@ -173,7 +185,12 @@ class User:
             return self.nick
         return self.writer.get_extra_info('peername')[0]
 
-    async def serve(self):
+    async def read_lines(self):
+        """
+        Read messages until (i) the user send a QUIT command or (ii) the
+        user close his side of the connection (FIN sent) or (iii) the
+        connection is reset.
+        """
         buffer = b""
         while type(self.state) != UserQuitState:
             try:
@@ -183,39 +200,61 @@ class User:
             if not chunk:
                 break
 
+            # imagine two messages of 768 bytes are sent together, read(1024)
+            # only read the first one and the 256 first bytes of the second,
+            # the first ends up in lines, the second in buffer. Next call to
+            # read(1024) will complete the second message.
             buffer += chunk
             *lines, buffer = buffer.split(b'\r\n')
-
-            if len(buffer) > 1024:
+            if any(len(line) > 1022 for line in lines + [buffer]):
+                # one IRC message cannot exceed 1024 bytes (\r\n included)
                 raise MemoryError("Message exceed 1024 bytes")
 
             for line in lines:
-                logger.msg("%s < %s", self, line)
-                cmd, *args = line.decode().split()
-                func = getattr(self.state, cmd, None)
-                if getattr(func, 'command', False):
-                    try:
-                        await func(args)
-                    except IRCException as exc:
-                        logger.warning("%s sent an invalid command.", self)
-                        await self.send(exc.args[0])
-                else:
-                    logger.warning("%s sent an unknown command: %s", self, cmd)
+                yield line.decode()
 
+        # gracefull disconnection
         self.writer.write_eof()
         await self.writer.drain()
 
-    async def send(self, msg):
+    async def serve(self):
+        """ Listen for new messages and process them """
+        async for line in self.read_lines():
+            logger.log(IOLevel, "%s < %s", self, line)
+            cmd, *args = line.split()
+            func = getattr(self.state, cmd, None)
+            if getattr(func, 'command', False):
+                try:
+                    await func(args)
+                except IRCException as exc:
+                    logger.warning("%s sent an invalid command.", self)
+                    await self.send(exc.args[0])
+            else:
+                logger.warning("%s sent an unknown command: %s", self, cmd)
+
+    async def send(self, msg: str):
+        """ Send a message to the user """
         self.writer.write(f"{msg}\r\n".encode())
-        logger.msg("%s > %s", self, msg)
+        logger.log(IOLevel, "%s > %s", self, msg)
         await self.writer.drain()
 
 
 def command(func):
+    """ Denote the function is triggered by an IRC message """
     func.command = True
     return func
 
 class UserMetaState(metaclass=ABCMeta):
+    """
+    IRC State Machine, user starts Connected then Registed then Quit,
+    commands have different meaning in every of those states.
+    """
+
+    # The State instances implement a proxy to their user. You may use
+    # any User attribute or method as they were directly defined on the
+    # state. Beware to use ``self._user`` when transitionning between
+    # states.
+
     def __init__(self, user):
         object.__setattr__(self, "_user", user)
 
@@ -249,12 +288,18 @@ class UserMetaState(metaclass=ABCMeta):
 
     @command
     async def QUIT(self, args):
+        msg = " ".join(args) if args else ":Disconnected"
+        for channel in self.channels:
+            channel.users.remove(self._user)
+            await channel.send_all(f":{self.nick} QUIT {msg}")
+        self.channels.clear()
         self.state = UserQuitState(self._user)
 
 
 class UserConnectedState(UserMetaState):
     """
-    TCP connection established but user not registered yet
+    The user is just connected, he must register via the NICK command
+    first before going on.
     """
 
     @command
@@ -284,7 +329,8 @@ class UserConnectedState(UserMetaState):
 
 class UserRegisteredState(UserMetaState):
     """
-    User connected and registered
+    The user sent the NICK command, he is fully registered to the server
+    and may use any command.
     """
 
     @command
@@ -346,18 +392,10 @@ class UserRegisteredState(UserMetaState):
                 raise ErrErroneusNickname(user)
             await receiver.send(f":{self.nick} PRIVMSG {user} {msg}")
 
-    @command
-    async def QUIT(self, args):
-        msg = " ".join(args) if args else ":Disconnected"
-        for channel in self.channels:
-            channel.users.remove(self._user)
-            await channel.send_all(f":{self.nick} QUIT {msg}")
-        self.state = UserQuitState(self._user)
-
 
 class UserQuitState(UserMetaState):
     """
-    User sent QUIT command
+    The user sent the QUIT command, no more message should be processed
     """
 
     @command
@@ -382,6 +420,11 @@ class UserQuitState(UserMetaState):
 
 
 class IRCException(Exception):
+    """
+    Abstract IRC exception
+
+    They are excepted by serve() and forwarded to the user.
+    """
     def __init__(self, *args):
         super().__init__(type(self).format(*args))
 
@@ -391,7 +434,6 @@ class IRCException(Exception):
             code=cls.code,
             error=cls.msg % args,
         )
-
 
 class ErrNoNicknameGiven(IRCException):
     msg = ":No nickname given"

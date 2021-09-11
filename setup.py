5cc0601209d325303cb5b5e3c5c111d386746619
Julien Castiaux <julien.castiaux@gmail.com>
2021-09-12 01:56:48 +0200

    fix: setup was still v1

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 from setuptools import setup
 
-setup(
-    py_modules=['aioircd'],
-    entry_points={
-        'console_scripts': [
-            'aioircd = aioircd:main'
-        ],
-    },
-)
+setup()
